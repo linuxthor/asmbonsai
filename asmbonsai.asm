5a9abebb54de7583e8a93e78e5e0396974cedacc
linuxthor <linuxthor@users.noreply.github.com>
2023-12-23 09:33:35 +0000

    Update asmbonsai.asm

--- asmbonsai.asm
+++ asmbonsai.asm
@@ -5,10 +5,10 @@ ehdr:
     db 0x7f, "ELF"                  ; 4      e_ident / jg _start 
 
 more:
-    xchg rsi, rcx                   ; 3      rsi is 'near enough' the buffer now
-    pop rax                         ; 1      only works if argc is 1 :(  
-    mov dl, 60                      ; 2      
-    jmp _end                        ; 2
+    xchg rsi, rcx                   ; 3
+    pop  rax                        ; 1
+    mov  dl, 60                     ; 2
+    jmp  _start                     ; 2
 
     dd 0x01                         ; 4
     dw 3                            ; 2      e_type:    dynamic
@@ -18,13 +18,13 @@ more:
     dq 0x0c                         ; 8
 
 go:
-    syscall                         ; 2      read(0, NULL, 0) to get address in rcx     
+    syscall                         ; 2      
     jmp more                        ; 2
     db 0xf4                         ; 1  
     db 0xff                         ; 1
 
 message:
-    db "He"                         ; 2
+    db "He"                         ; 1
     dd 0x00                         ; 4
     db "ll"                         ; 2 
     dw 0x38                         ; 2
@@ -32,8 +32,5 @@ message:
     db 0x08,"o, world!",0x0a        ; 11     
  
 _start:
+    syscall                         ; 2
     jmp go                          ; 2
-
-_end: 
-    syscall                         ; 2
-    syscall                         ; 2
