f1403a12df937c1740d3966f9960feda2d5ea3eb
linuxthor <linuxthor@users.noreply.github.com>
2023-12-25 09:23:31 +0000

    Update asmbonsai.asm

--- asmbonsai.asm
+++ asmbonsai.asm
@@ -5,10 +5,12 @@ ehdr:
     db 0x7f, "ELF"                  ; 4      e_ident / jg _start 
 
 more:
-    mov rsi, rcx                    ; 3
-    pop rax                         ; 1
-    mov dl, 60                      ; 2
-    jmp _start                      ; 2
+    pop  rax                        ; 1
+    push rcx                        ; 1
+    pop  rsi                        ; 1
+    mov  dl, 60                     ; 2
+    push rdx                        ; 1
+    jmp  _start                     ; 2
 
     dd 0x01                         ; 4
     dw 3                            ; 2      e_type:    dynamic
@@ -18,7 +20,7 @@ more:
     dq 0x0c                         ; 8
 
 go:
-    syscall                         ; 2      
+    syscall                         ; 2    
     jmp more                        ; 2
     db 0xf4                         ; 1  
     db 0xff                         ; 1
@@ -30,7 +32,6 @@ message:
     dw 0x38                         ; 2
     dd 0x01                         ; 4
     db 0x08,"o, world!",0x0a        ; 11     
- 
+
 _start:
-    syscall                         ; 2
     jmp go                          ; 2
